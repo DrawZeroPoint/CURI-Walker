f84f6b4b210f867aac05e11ad98f844aa5bbd572
Miguel Fernandes <killazfern@gmail.com>
2020-07-09 13:49:18 +0200

    task 14 video

--- ubt_sim_ws/src/walker_movement/scripts/liftBox.py
+++ ubt_sim_ws/src/walker_movement/scripts/liftBox.py
@@ -11,14 +11,14 @@ import sys
 
 intermediate_left  = [[0.072,  0.32, -0.278],[-0.280, 0.650, -0.280, 0.649]]
 intermediate2_left  = [[0.250,  0.319, -0.263], [-0.281, 0.651, -0.279, 0.647]]
-grapPose_left  = [[0.250, 0.186, -0.273], [-0.450, 0.551, -0.444, 0.545]]
+grapPose_left  = [[0.255, 0.186, -0.273], [-0.450, 0.551, -0.444, 0.545]]
 
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('lift_box_py')
 scene = moveit_commander.PlanningSceneInterface(ns="/walker")
 wmu = WalkerMovementUtils(disable_gait=True)
 
-wmu.moveToJointDualArmSimmetrical([0,0,0,0,0,0,0])
+# wmu.moveToJointDualArmSimmetrical([0,0,0,0,0,0,0])
 wmu.releaseGrasp(True)
 wmu.releaseGrasp(False)
 
@@ -37,15 +37,14 @@ rospy.loginfo("Moving to grasp pose")
 
 wmu.moveToEeDualArmMirrored(grapPose_left[0],grapPose_left[1],do_cartesian=False)
 
-
 wmu.graspCan(True)
 wmu.graspCan(False)
 
 leftPose = wmu.getEePose(True)
 rightPose = wmu.getEePose(False)
-
-leftPoseL  = [[leftPose.pose.position.x,leftPose.pose.position.y,leftPose.pose.position.z+0.1],[leftPose.pose.orientation.x,leftPose.pose.orientation.y,leftPose.pose.orientation.z,leftPose.pose.orientation.w]]
-rightPoseL = [[rightPose.pose.position.x,rightPose.pose.position.y,rightPose.pose.position.z+0.1],[rightPose.pose.orientation.x,rightPose.pose.orientation.y,rightPose.pose.orientation.z,rightPose.pose.orientation.w]]
+lift = 0.1
+leftPoseL  = [[leftPose.pose.position.x,leftPose.pose.position.y,leftPose.pose.position.z+lift],[leftPose.pose.orientation.x,leftPose.pose.orientation.y,leftPose.pose.orientation.z,leftPose.pose.orientation.w]]
+rightPoseL = [[rightPose.pose.position.x,rightPose.pose.position.y,rightPose.pose.position.z+lift],[rightPose.pose.orientation.x,rightPose.pose.orientation.y,rightPose.pose.orientation.z,rightPose.pose.orientation.w]]
 
 rospy.loginfo("Moving up")
 wmu.moveToEeDualArmMirrored(leftPoseL[0],leftPoseL[1],do_cartesian=True)
--- ubt_sim_ws/src/walker_walk/scripts/mat_pyth.py
+++ ubt_sim_ws/src/walker_walk/scripts/mat_pyth.py
@@ -6,6 +6,7 @@ import actionlib
 import walker_movement.msg
 from geometry_msgs.msg import PoseStamped
 import rospkg
+import numpy as np
 
 def buildPoseStamped(position_xyz, orientation_xyzw, frame_id):
     pose = PoseStamped()
@@ -37,7 +38,7 @@ rospy.loginfo("Action clients connected")
 
 
 
-inputFile = rospkg.RosPack().get_path('walker_walk')+"/data/backwards_lowvel_lowheight.mat"
+inputFile = rospkg.RosPack().get_path('walker_walk')+"/data/backwards_24steps_5cm_sloooow.mat"
 x = loadmat(inputFile)["footinfos"]
 rospy.loginfo("Loaded Matlab data from "+inputFile)
 y = x[0]
@@ -51,7 +52,7 @@ full_foot_right_y = []
 full_foot_right_z = []
 
 time_dilat = 0
-
+trig = True
 for elem in y:
     time_vector =elem["timevec"][0][0][0][1:]
 
@@ -70,13 +71,19 @@ for elem in y:
     foot_left_z     = foot_left[4][1:] #[-1]
     foot_left_z_vel = foot_left[5][1:] #[-1]
 
+    dist_value = 0.10
+    if trig:
+        ivo_aux = np.arange(0,dist_value,(dist_value/len(foot_left_x)))
+        trig = False
+    else:
+        ivo_aux = np.full(len(foot_left_x),dist_value)
     for aux in foot_left_x:
         full_foot_left_x.append(aux)
-    for aux in foot_left_y:
-        full_foot_left_y.append(aux)
+    for aux,altaux in zip(foot_left_y,ivo_aux):
+        full_foot_left_y.append(aux+altaux)
+        # print(aux+altaux)
     for aux in foot_left_z:
         full_foot_left_z.append(aux)
-
     foot_right_x     = foot_right[0][1:] #[-1]
     foot_right_x_vel = foot_right[1][1:] #[-1]
     foot_right_y     = foot_right[2][1:] #[-1]
@@ -86,13 +93,10 @@ for elem in y:
 
     for aux in foot_right_x:
         full_foot_right_x.append(aux)
-    for aux in foot_right_y:
-        full_foot_right_y.append(aux)
+    for aux,altaux in zip(foot_right_y,ivo_aux):
+        full_foot_right_y.append(aux+altaux)
     for aux in foot_right_z:
         full_foot_right_z.append(aux)
-
-
-
 
 
 def rand_functio():
@@ -114,17 +118,17 @@ def rand_functio():
     poses_left = []
     poses_right = []
     for x,y,z in zip(full_foot_left_x,full_foot_left_y,full_foot_left_z):
-        poses_left.append(buildPoseStamped([x,y,z],[0,0,0.793,0.609],"center_of_mass"))
+        poses_left.append(buildPoseStamped([x,y,z],[0,0,0.707,0.707],"center_of_mass")) #[0,0,0.793,0.609]
 
     for x,y,z in zip(full_foot_right_x,full_foot_right_y,full_foot_right_z):
-        poses_right.append(buildPoseStamped([x,y,z],[0,0,0.609,0.793],"center_of_mass"))
+        poses_right.append(buildPoseStamped([x,y,z],[0,0,0.707,0.707],"center_of_mass")) #[0,0,0.609,0.793]
         pass
 
     goal.times_from_start_left = duration
     goal.times_from_start_right  = duration
     goal.poses_left = poses_left #[buildPoseStamped([foot_left_x,foot_left_y,foot_left_z],[0,0,0.707,0.707],"center_of_mass")]
     goal.poses_right = poses_right #[buildPoseStamped([foot_right_x,foot_right_y,foot_right_z],[0,0,0.707,0.707],"center_of_mass")]
-    print(goal)
+    # print(goal)
     # exit(1)
     rospy.loginfo("times_from_start_left[0]="+str(goal.times_from_start_left[0]))
     rospy.loginfo("times_from_start_right[0]="+str(goal.times_from_start_right[0]))
